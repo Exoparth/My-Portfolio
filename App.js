b3b43c81ae182eeedafd28f36af3501e46428ab1
Parth Panchal <parthpanchal1352003@gmail.com>
2024-10-05 20:52:52 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,8 @@
+let downloadbtn = document.getElementById("download");
+let git = document.getElementById("git");
+downloadbtn.addEventListener("click", () => {
+  window.open("Resume.pdf");
+});
+git.addEventListener("click",()=>{
+    window.open("https://github.com/Exoparth")
+})
